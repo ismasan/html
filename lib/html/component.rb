de78d6d2a0f21e3d7b7d24eeaac066d7e7ca6612
Ismael Celis <ismaelct@gmail.com>
2020-10-05 22:26:53 +0100

    First pass at components

--- lib/html/component.rb
+++ lib/html/component.rb
@@ -2,8 +2,26 @@
 
 module HTML
   class Component
-    def self.render(*args)
-      new(*args).to_s
+    def self.props
+      @props ||= {}
+    end
+
+    def self.prop(key)
+      props[key] = true
+    end
+
+    def self.render(*args, &block)
+      new(*args, &block).to_s
+    end
+
+    def initialize(props = {}, &block)
+      @props = self.class.props.keys.each.with_object({}) do |key, ret|
+        raise ArgumentError, "expects #{key}" unless props.key?(key)
+
+        ret[key] = props[key]
+      end
+
+      @content_block = block_given? ? block : nil
     end
 
     def to_s
@@ -12,8 +30,14 @@ module HTML
 
     private
 
+    attr_reader :props, :content_block
+
     def render
       ''
+    end
+
+    def content
+      content_block ? content_block.call(self) : nil
     end
 
     def tag(*args, &block)
--- spec/component_spec.rb
+++ spec/component_spec.rb
@@ -3,13 +3,12 @@
 RSpec.describe HTML::Component do
   specify do
     Input = Class.new(described_class) do
-      def initialize(name, value)
-        @name, @value = name, value
-      end
+      prop :name
+      prop :value
 
       def render
         tag :div, class: 'input' do |c|
-          c.tag :input, type: 'text', name: @name, value: @value
+          c.tag :input, type: 'text', name: props[:name], value: props[:value]
         end
       end
     end
@@ -17,12 +16,33 @@ RSpec.describe HTML::Component do
     row = Class.new(described_class) do
       def render
         tag :div, class: 'row' do |c|
-          c.tag Input.new('email', 'lol@ca.cl')
+          c.tag Input.new(name: 'email', value: 'lol@ca.cl')
         end
       end
     end
 
-    expect(Input.render('email', 'email@me.cl')).to eq(%(<div class="input">\n<input type="text" name="email" value="email@me.cl" />\n</div>))
+    expect(Input.render(name: 'email', value: 'email@me.cl')).to eq(%(<div class="input">\n<input type="text" name="email" value="email@me.cl" />\n</div>))
     expect(row.render).to eq(%(<div class="row">\n<div class="input">\n<input type="text" name="email" value="lol@ca.cl" />\n</div>\n</div>))
+  end
+
+  specify 'nested content' do
+    Parent = Class.new(described_class) do
+      prop :title
+
+      def render
+        tag :div, class: 'parent' do |c|
+          c.tag :h1, props[:title]
+          c.tag :p do |c|
+            content
+          end
+        end
+      end
+    end
+
+    output = Parent.render(title: 'Parent') do |c|
+      'Content here'
+    end
+
+    expect(output).to eq(%(<div class="parent">\n<h1>Parent</h1>\n<p>\nContent here\n</p>\n</div>))
   end
 end
